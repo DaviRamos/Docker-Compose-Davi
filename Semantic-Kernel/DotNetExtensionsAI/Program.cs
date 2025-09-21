a9e4a4180d8fb3c58942c51354c2abe670ac9c00
DaviRamos <davi.info@gmail.com>
2025-09-21 06:39:15 -0300

    semanticKerNel ADD

--- Semantic-Kernel/DotNetExtensionsAI/Program.cs
+++ Semantic-Kernel/DotNetExtensionsAI/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.AI;
+using OpenAI.Chat;
+
+const string openAIKey = "Hello, World!";
+var ollamUrl = new Uri("http://localhost:11434");
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var cache = new MemoryDistributedCache(Options.Create(new MemoryDistributedCacheOptions()));
+
+var client = app.Environment.IsDevelopment()
+    ? new OllamaClient(ollamUrl, "phi3:latest")
+    : new OpenAI.Chat.ChatClient("gpt-40-mini", openAIKey)
+        .AsIChatClient();
+
+var cachedClient = new ChatClientBuilder(client)
+    .UseDistributedCache(cache)
+    .Build();
+
+app.MapPost("/", async (Question question) =>
+{
+    var result = await cachedClient.GetResponseAsync(question.Prompt);
+    return Results.Ok(result.Text);
+});
+
+
+app.MapPost("/v2", async (Question question) =>
+{
+    var result = await client.GetResponseAsync(
+    [
+        new ChatMessage(ChatRole.System,
+        "You're weather expert,answer me in ust one setences, whitin 50 words"),
+
+            new ChatMessage(ChatRole.User, question.Prompt)
+        ]
+    );
+    return Results.Ok(result.Text);
+});
+
+app.MapPost("/v3", async(Question question) =>
+{
+    var result = await cachedClient.GetResponseAsync(
+    [
+        new ChatMessage(ChatRole.System,
+        "You're weather expert,answer me in ust one setences, whitin 50 words"),
+
+            new ChatMessage(ChatRole.User, question.Prompt)
+        ]
+    );
+    return Results.Ok(result.Text); 
+});
+
+
+app.Run();
+
+public record Question(string Prompt);
--- Semantic-Kernel/SemanticKernel1/Program.cs
+++ Semantic-Kernel/SemanticKernel1/Program.cs
@@ -1,4 +1,32 @@
-﻿using Microsoft.SemanticKernel;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.Ollama;
 
 var builder = Kernel.CreateBuilder()
-    .AddOl
+    .AddOllamaChatCompletion(
+        modelid: "llama3.1:latest",
+        endpoint: new Uri("http://localhost:11434")
+        )
+    .Build();
+
+//Enterprise Components
+builder.Services.AddLogging(x 
+    => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+var app = builder.Build();
+
+var chatCompletionService = app.GetRequiredService<IChatCompletion>();
+
+string? input;
+do
+{
+    Console.Write("User > ");
+    input = Console.ReadLine();
+
+    var result = chatCompletionService
+    .GetChatMessagesAsync(input, Kernel: app);
+
+    Console.WriteLine("Assistant > " + result);
+    
+} while (input is null);
